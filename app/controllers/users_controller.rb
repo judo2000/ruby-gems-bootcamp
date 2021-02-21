7464a5cd26836476af116cb9072b37be1d2dbc43
Judo2000 <judo2000@comcast.net>
2021-02-21 08:02:43 +0000

    Add lockable and confirmable to devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController 
+  
+  def index 
+    @users = User.all.order(created_at: :asc)
+  end
+  
+  def show 
+    @user = User.find(params[:id])
+  end
+  
+  def ban 
+    @user = User.find(params[:id])
+    if @user.access_locked? 
+      @user.unlock_access!
+    else
+      @user.lock_access!
+    end
+    redirect_to @user, notice: "User was ban accessed locked: #{@user.access_locked?}"
+  end 
+  
+  def destroy 
+    @user = User.find(params[:id])
+    @user.destroy 
+    redirect_to users_path, notice: "User was successfully destroyed"
+  end
+  
+end
